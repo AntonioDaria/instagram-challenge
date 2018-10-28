c8148d4120b41834850d42eb80895c28ef33bc86
Antonio <antoniodaria@msn.com>
2018-10-28 13:07:41 +0000

    Edit, Update and Destroy Actions and View Files

--- spec/features/user_can_submit_pics_spec.rb
+++ spec/features/user_can_submit_pics_spec.rb
@@ -10,4 +10,16 @@ RSpec.feature "View pictures", type: :feature do
     expect(page).to have_content("testtitle1")
   end
 
+  pending "Can check that the newest pics appears first" do
+    visit "/pics/new"
+    fill_in "pic_title", with: "testtitle1"
+    fill_in "pic_description", with: "testdescription1"
+    click_button 'btn btn-info'
+    visit "/pic/new"
+    fill_in "pic_title", with: "testtitle2"
+    fill_in "pic_description", with: "testdescription2"
+    click_button 'btn btn-info'
+    expect(page).to have_content("testtitle1 " + " " + "testtitle2")
+ end
+
 end
