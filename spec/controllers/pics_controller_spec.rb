dd67fe7a51bc46ab41e3d036135505559e40c672
Antonio <antoniodaria@msn.com>
2018-10-28 11:41:08 +0000

    Testing pics_controller

--- spec/controllers/pics_controller_spec.rb
+++ spec/controllers/pics_controller_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe PicsController, type: :controller do
 
+  describe "GET /new " do
+    it "responds with 200" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /" do
+    pending "responds with 200" do
+      pic :create, params: { pic: { description: "testdescription"} }
+      expect(response).to redirect_to(pics_url)
+    end
+
+    pending "creates a pic" do
+      pic :create, params: { pic: { description: "testdescription"} }
+      expect(Pic.find_by(description: "testdescription")).to be
+    end
+  end
+
+
+
+
+
+    describe "GET /" do
+    it "responds with 200" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
+
+
 end
--- spec/features/user_can_submit_pics_spec.rb
+++ spec/features/user_can_submit_pics_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature "Timeline", type: :feature do
+RSpec.feature "View pictures", type: :feature do
 
-  scenario "Can submit pictures and view them" do
+  pending "Can submit pictures and view them" do
     visit "/pics/new"
     fill_in "pic_title", with: "testtitle1"
     fill_in "pic_description", with: "testdescription1"
-    click_button 'btn btn-info' 
+    click_button 'btn btn-info'
     expect(page).to have_content("testtitle1")
   end
 
